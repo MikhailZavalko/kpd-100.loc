57ecab73c82c807585fb9f7ce2a79329e9b6b643
MikhailZavalko <mikhailzavalko@gmail.com>
2020-09-18 17:16:22 +1000

    try to add pinterest gallery on new page

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,4 @@
-$(window).on('load', function() {
+$(window).on('load', function () {
 
 
 
@@ -20,10 +20,10 @@ $(window).on('load', function() {
 
 
 	/* JS FOR SPOILER */
-	$( '.spoiler' ).on( 'click', function(e) {
+	$('.spoiler').on('click', function (e) {
 		e.preventDefault();
-		$( '.spoiler-wrap' ).css( 'display', 'none' );
-		$( '.spoiler' ).css( 'display', 'block' );
+		$('.spoiler-wrap').css('display', 'none');
+		$('.spoiler').css('display', 'block');
 	});
 	/* JS FOR SPOILER */
 
@@ -36,41 +36,41 @@ $(window).on('load', function() {
 
 
 	/* YANDEX MAPS SETTINGS */
-	ymaps.ready(function() {
+	ymaps.ready(function () {
 
-		if( document.getElementById('map') ) {
-			var plant = new ymaps.Map("map",{
+		if (document.getElementById('map')) {
+			var plant = new ymaps.Map("map", {
 				center: [48.458623, 135.129240],
 				zoom: 16,
 				controls: ['zoomControl'],
 				behaviors: ["drag", "dblClickZoom"]
 			}),
-			plantMarker = new ymaps.Placemark([48.458623, 135.129240],{ hintContent: "г. Хабаровск, ул. Иркутская, 8" },
-			{
-				iconLayout: "default#image",
-				iconImageHref: "img/marker_1.png",
-				iconImageSize: [423, 84],
-				iconImageOffset: [-404, -84]
-			});
+				plantMarker = new ymaps.Placemark([48.458623, 135.129240], { hintContent: "г. Хабаровск, ул. Иркутская, 8" },
+					{
+						iconLayout: "default#image",
+						iconImageHref: "img/marker_1.png",
+						iconImageSize: [423, 84],
+						iconImageOffset: [-404, -84]
+					});
 			plant.geoObjects.add(plantMarker);
 		}
 
-		
 
-		if( document.getElementById('map-sales') ) {
-			var salesDep = new ymaps.Map("map-sales",{
+
+		if (document.getElementById('map-sales')) {
+			var salesDep = new ymaps.Map("map-sales", {
 				center: [48.471580, 135.066150],
 				zoom: 16,
 				controls: ['zoomControl'],
 				behaviors: ["drag", "dblClickZoom"]
 			}),
-			depMarker = new ymaps.Placemark([48.471580, 135.066150],{ hintContent: "г. Хабаровск, ул. Калинина, 51" },
-			{
-				iconLayout: "default#image",
-				iconImageHref: "img/marker_2.png",
-				iconImageSize: [430, 128],
-				iconImageOffset: [-22, -128]
-			});
+				depMarker = new ymaps.Placemark([48.471580, 135.066150], { hintContent: "г. Хабаровск, ул. Калинина, 51" },
+					{
+						iconLayout: "default#image",
+						iconImageHref: "img/marker_2.png",
+						iconImageSize: [430, 128],
+						iconImageOffset: [-22, -128]
+					});
 			salesDep.geoObjects.add(depMarker);
 		}
 
@@ -95,9 +95,9 @@ $(window).on('load', function() {
 		removalDelay: 300,
 		mainClass: 'my-mfp-slide-bottom',
 		callbacks: {
-			beforeOpen: function() {
+			beforeOpen: function () {
 				var $triggerEl = $(this.st.el),
-				newClass = $triggerEl.data("modal-class");
+					newClass = $triggerEl.data("modal-class");
 				if (newClass) {
 					this.st.mainClass = this.st.mainClass + ' ' + newClass;
 				}
@@ -108,8 +108,41 @@ $(window).on('load', function() {
 
 
 
+
+	/* MASONRY GALLERY */
+	function resizeGridItem(item) {
+		grid = document.getElementsByClassName("masonry")[0];
+		rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+		rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+		rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+		item.style.gridRowEnd = "span " + rowSpan;
+	}
+
+	function resizeAllGridItems() {
+		allItems = document.getElementsByClassName("item");
+		for (x = 0; x < allItems.length; x++) {
+			resizeGridItem(allItems[x]);
+		}
+	}
+
+	function resizeInstance(instance) {
+		item = instance.elements[0];
+		resizeGridItem(item);
+	}
+
+	window.onload = resizeAllGridItems();
+	window.addEventListener("resize", resizeAllGridItems);
+
+	allItems = document.getElementsByClassName("item");
+	for (x = 0; x < allItems.length; x++) {
+		imagesLoaded(allItems[x], resizeInstance);
+	}
+	/* MASONRY GALLERY */
+
+
+
 	/* E-mail Ajax Send */
-	$("form").submit(function(e) {
+	$("form").submit(function (e) {
 		e.preventDefault;
 		var th = $(this);
 		$('.form').addClass('loading');
@@ -117,10 +150,10 @@ $(window).on('load', function() {
 			type: "POST",
 			url: "../send.php",
 			data: th.serialize()
-		}).done(function() {
+		}).done(function () {
 			$('.form').removeClass('loading');
 			$(".success").addClass("visible");
-			setTimeout(function() {
+			setTimeout(function () {
 				// Done Functions
 				th.trigger("reset");
 				$(".success").removeClass("visible");
